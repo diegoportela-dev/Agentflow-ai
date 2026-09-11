e9ae25ede22e5ece4f644a5c925010aef28ea6fb
Diego <diego.portela@empresagrafica.com.br>
2026-09-11 18:37:58 -0300

    refactor: centraliza configuracoes do supervisor

--- supervisor/src/infrastructure/dependencies.py
+++ supervisor/src/infrastructure/dependencies.py
@@ -2,16 +2,16 @@ from src.domain.agent_gateway import AgentGateway
 from src.domain.agent_routing_strategy import AgentRoutingStrategy
 from src.infrastructure.a2a_agent_gateway import A2AAgentGateway
 from src.infrastructure.llm_routing_strategy import LLMRoutingStrategy
-
-
-AGENTS = {
-    "cartao_credito": "http://cartao_credito_agent:8000",
-    "abrir_conta": "http://abrir_conta_agent:8000",
-}
+from src.infrastructure.settings import settings
 
 
 def get_agent_gateway() -> AgentGateway:
-    return A2AAgentGateway(AGENTS)
+    agents = {
+        "cartao_credito": settings.cartao_credito_agent_url,
+        "abrir_conta": settings.abrir_conta_agent_url,
+    }
+
+    return A2AAgentGateway(agents)
 
 
 def get_agent_routing_strategy() -> AgentRoutingStrategy:
---
+++ supervisor/src/infrastructure/settings.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Settings:
+    def __init__(self):
+        self.cartao_credito_agent_url = os.getenv(
+            "CARTAO_CREDITO_AGENT_URL",
+            "http://cartao_credito_agent:8000",
+        )
+
+        self.abrir_conta_agent_url = os.getenv(
+            "ABRIR_CONTA_AGENT_URL",
+            "http://abrir_conta_agent:8000",
+        )
+
+
+settings = Settings()
